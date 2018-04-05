34e17086d3d396cdf4f2679b027e960ad7331264
wangwenxiao <zjdxwwx@163.com>
2018-04-05 11:12:45 +0800

    add read data file

---
+++ read_data.py
@@ -0,0 +1,43 @@
+import scipy.io
+
+def read_data(start, num, train=True):
+    if train:
+        file_index = int(start / 60000) + 1
+        from_index = start % 60000
+        data = scipy.io.loadmat("training_and_validation_batches/" + str(file_index) + ".mat")
+    else:
+        file_index = int(start / 10000) + 1
+        from_index = start % 10000
+        data = scipy.io.loadmat("test_batches/" + str(file_index) + ".mat")
+    data = data.get("affNISTdata")["image"]
+    return data[0][0][:, from_index: from_index+num]
+
+def read_labels(start, num, train=True, one_of_n=True):
+    if train:
+        file_index = int(start / 60000) + 1
+        from_index = start % 60000
+        data = scipy.io.loadmat("training_and_validation_batches/" + str(file_index) + ".mat")
+    else:
+        file_index = int(start / 10000) + 1
+        from_index = start % 10000
+        data = scipy.io.loadmat("test_batches/" + str(file_index) + ".mat")
+    if one_of_n:
+        data = data.get("affNISTdata")["label_one_of_n"]
+        return data[0][0][:, from_index: from_index+num]
+    else:
+        data = data.get("affNISTdata")["label_int"]
+        return data[0][0][0][from_index: from_index+num]
+
+
+if __name__ == "__main__":
+    train_data = read_data(0, 100)
+    test_data = read_data(0, 100, train=False)
+
+    train_labels = read_labels(0, 100)
+    test_labels = read_labels(0, 100, train=False, one_of_n=False)
+
+    print(train_data)
+    print(test_data)
+    print(train_labels)
+    print(test_labels)
+
